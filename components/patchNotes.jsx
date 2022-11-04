821f23b6f0f99afbb8a69a883915944d85caa6ba
Brian Joseph Studio <brianjosephstudio@gmail.com>
2022-11-04 16:27:25 +0100

    Release 3.5.1

--- components/patchNotes.jsx
+++ components/patchNotes.jsx
@@ -1,5 +1,5 @@
 var patchNotesBodyText =
-"Version 3.5.0\n\
+"Version 3.5.1\n\
 -Resources Manager:\
     There's a new Resources Manager in Project Utilities. You can use this tool to import most of our resources individually.\
     Not all resources are currently available, this is an ongoing project, however all necesary files for any of our Animator Hub Templates can be downloaded form here.";
