2de63d8ae232197d9d3404954db50138fc0c78a7
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-04-25 12:14:51 +0200

    Release 3.15.2

--- Animator_Hub/components/patchNotes.jsx
+++ Animator_Hub/components/patchNotes.jsx
@@ -1,5 +1,5 @@
 var patchNotesBodyText =
-"Version 3.15.1\n\
+"Version 3.15.2\n\
 -Map Overviews workspace reworked:\
     The Map Overviews panel was redesigned to be visually appealing, take less space, and create room for more tools. Alt + double click on the map thumbnails to open tutorial.\
 -New Map Highlights Tool:\
